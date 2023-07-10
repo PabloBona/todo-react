f26972dbd3ca35ac6e147c2dee8d7717ab08be75
PabloBona <pablobonasera@gmail.com>
2023-07-10 10:04:08 -0300

    Fix linters

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
- 
 import './App.css';
-import TodoApp from './components/TodoApp';
 
 function App() {
   return (
     <div className="App">
-        <TodoApp />
+      <App />
     </div>
   );
 }
